72a27f365abc7c729a9b590c41ceccd8e6541c1f
sunnyinspired <sundayelesi@gmail.com>
2023-11-16 00:32:14 +0100

    updated baseurl

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = nextConfig
 
 module.exports = {
     env: {
-      baseURL: 'http://41.242.50.194:2090/cdr/api/',
+      baseURL: 'http://localhost:9091/cdr/api/',
       NEXTAUTH_SECRET: '71OJ4KWqjyqR9yGYp5EXKmmr0VKEIZaMNn6ReaMLdcqg=',
       NEXTAUTH_URL: 'http://localhost:3000'
     },
