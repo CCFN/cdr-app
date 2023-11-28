24515ff852a20157cf74cba792b2c2df66d862e9
sunnyinspired <sundayelesi@gmail.com>
2023-11-28 22:19:22 +0100

    Updated base url

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = nextConfig
 
 module.exports = {
     env: {
-      baseURL: 'http://stateserver.org:8080/cdr/api/',
+      baseURL: 'https://api.stateserver.org/cdr/api/',
       NEXTAUTH_SECRET: '71OJ4KWqjyqR9yGYp5EXKmmr0VKEIZaMNn6ReaMLdcqg=',
       NEXTAUTH_URL: 'https://stateserver.org'
     },
